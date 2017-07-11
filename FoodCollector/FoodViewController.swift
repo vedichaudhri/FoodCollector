a447574d05ed1113ccd1616578f7ff590894269e
Vedi Chaudhri <vedichaudhri@gmail.com>
2017-07-11 15:54:28 -0700

    image picker controller works

--- FoodCollector/FoodViewController.swift
+++ FoodCollector/FoodViewController.swift
@@ -8,28 +8,33 @@
 
 import UIKit
 
-class FoodViewController: UIViewController {
+class FoodViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var foodImageView: UIImageView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        //allows another view controller to pop up on the screen
+        present(imagePicker, animated: true, completion: nil)
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+ 
+    @IBAction func cameraTapped(_ sender: Any) {
     }
-    */
 
+    @IBAction func addTapped(_ sender: Any) {
+    }
 }
